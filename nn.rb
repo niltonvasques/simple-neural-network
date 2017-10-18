1b4c06defed94eea9ca504f367f0f142f32cd68e
niltonvasques <nilton.vasques@gmail.com>
2017-10-18 01:36:56 -0300

    Add H letter

--- nn.rb
+++ nn.rb
@@ -94,6 +94,7 @@ def print_prediction(outputs)
   puts "E: #{(outputs[4].round(2) * 100).to_i}%"
   puts "F: #{(outputs[5].round(2) * 100).to_i}%"
   puts "G: #{(outputs[6].round(2) * 100).to_i}%"
+  puts "H: #{(outputs[7].round(2) * 100).to_i}%"
 end
 
 def print_letter(letter)
@@ -111,72 +112,61 @@ end
 nn = Network.new(
   Layer.new(20, 20),
   Layer.new(10, 20),
-  Layer.new(7, 10)
+  Layer.new(8, 10)
 )
 
 # training
 
 TRAINING_DATA = [
-  { letter: LETTER_A.to_v, expected: EXPECTED_A.to_v },
-  { letter: LETTER_B.to_v, expected: EXPECTED_B.to_v },
-  { letter: LETTER_C.to_v, expected: EXPECTED_C.to_v },
-  { letter: LETTER_D.to_v, expected: EXPECTED_D.to_v },
-  { letter: LETTER_E.to_v, expected: EXPECTED_E.to_v },
-  { letter: LETTER_F.to_v, expected: EXPECTED_F.to_v },
-  { letter: LETTER_G.to_v, expected: EXPECTED_G.to_v },
+  { letter: 'A', sample: LETTER_A.to_v, expected: EXPECTED_A.to_v },
+  { letter: 'B', sample: LETTER_B.to_v, expected: EXPECTED_B.to_v },
+  { letter: 'C', sample: LETTER_C.to_v, expected: EXPECTED_C.to_v },
+  { letter: 'D', sample: LETTER_D.to_v, expected: EXPECTED_D.to_v },
+  { letter: 'E', sample: LETTER_E.to_v, expected: EXPECTED_E.to_v },
+  { letter: 'F', sample: LETTER_F.to_v, expected: EXPECTED_F.to_v },
+  { letter: 'G', sample: LETTER_G.to_v, expected: EXPECTED_G.to_v },
+  { letter: 'H', sample: LETTER_H.to_v, expected: EXPECTED_H.to_v },
 ];
 
 1000.times do
   TRAINING_DATA.each do |sample|
-    output = nn.forward(sample[:letter])
+    output = nn.forward(sample[:sample])
     errors = output - sample[:expected]
     nn.back_propagate(errors)
   end
 end
 
-# test data
+# testing with training data
 
-ap "---------------- A --------------------"
-output = nn.forward(LETTER_A.to_v)
-print_prediction(output)
-ap "---------------------------------------"
+TRAINING_DATA.each do |data|
+  ap "---------------- #{data[:letter]} --------------------"
+  output = nn.forward(data[:sample])
+  print_prediction(output)
+  ap "---------------------------------------"
+end
 
-ap "---------------- B --------------------"
-output = nn.forward(LETTER_B.to_v)
-print_prediction(output)
-ap "---------------------------------------"
-
-ap "---------------- C --------------------"
-output = nn.forward(LETTER_C.to_v)
-print_prediction(output)
-ap "---------------------------------------"
-
-ap "---------------- D --------------------"
-output = nn.forward(LETTER_D.to_v)
-print_prediction(output)
-ap "---------------------------------------"
-
-ap "---------------- E --------------------"
-output = nn.forward(LETTER_E.to_v)
-print_prediction(output)
-ap "---------------------------------------"
-
-ap "---------------- F --------------------"
-output = nn.forward(LETTER_F.to_v)
-print_prediction(output)
-ap "---------------------------------------"
-
-ap "---------------- G --------------------"
-output = nn.forward(LETTER_G.to_v)
-print_prediction(output)
-ap "---------------------------------------"
-
+# testing with some random data
+#
 letter = [
   1, 1, 1, 0,
   1, 0, 0, 0,
   1, 0, 0, 0,
   1, 0, 0, 0,
   1, 1, 1, 1
+];
+
+print_letter(letter)
+ap "---------------- ? --------------------"
+output = nn.forward(letter.to_v)
+print_prediction(output)
+ap "---------------------------------------"
+
+letter = [
+  1, 1, 1, 1,
+  1, 0, 0, 0,
+  1, 1, 1, 0,
+  1, 0, 0, 0,
+  1, 0, 0, 0
 ];
 
 print_letter(letter)
